3d33de1594aa6afc0448bcc1d1102120a4dcf50e
Victor Thibert <victor.thibert@gmail.com>
2016-10-13 23:06:57 -0400

    disclaimer accordion

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+        this.classList.toggle("active");
+        this.nextElementSibling.classList.toggle("show");
+    }
+}
