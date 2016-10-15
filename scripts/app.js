81bc45d888f1bdd9c494ddedf605a88ddeb0aeb4
Victor Thibert <victor.thibert@gmail.com>
2016-10-15 19:56:08 -0400

    new sectioning

--- scripts/app.js
+++ scripts/app.js
@@ -23,4 +23,11 @@ $(document).ready(function() {
         scrollTop: $("#limits-body").offset().top - 90},
         'slow');
     });
+
+    $('#calc').click(function(event) {
+      event.preventDefault();
+    $('html,body').animate({
+        scrollTop: $("#calc-body").offset().top - 90},
+        'slow');
+    });
 });
