85ce8d5f419e29f1c906b2c5a0ba49ebb393d718
Victor Thibert <victor.thibert@gmail.com>
2016-10-16 01:49:28 -0400

    javascript dates

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,17 @@
 var acc = document.getElementsByClassName("accordion");
 var i;
 
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; 
+var yyyy = today.getFullYear();
+ 
+
+var inputDOB = document.getElementById('DOB');
+var inputwithdrawals = document.getElementById('withdrawals');
+var inputnet = document.getElementById('net');
+var inputcontributionroom = document.getElementById('contributionroom');
+
 
 for (i = 0; i < acc.length; i++) {
     acc[i].onclick = function(){
